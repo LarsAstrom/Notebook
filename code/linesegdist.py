5d6b118b228a5323898dc37747087a5924225879
Jonatan Nilsson <nilsson.jonatan98@gmail.com>
2018-10-10 11:14:33 +0200

    Fix error in calculating intersection between line

--- code/linesegdist.py
+++ code/linesegdist.py
@@ -1,16 +1,16 @@
 from __future__ import division
+
 '''
-line is defined by (a,b,c) such that ax+by+c=0 is the equation of 
-the line. Segment is defined by (x1,y1,x2,y2), where the segment 
+line is defined by (a,b,c) such that ax+by+c=0 is the equation of
+the line. Segment is defined by (x1,y1,x2,y2), where the segment
 is from (x1,y1) to (x2,y2). Point is defined by (x,y).
 
-if (a,b,c) is returned, then the solution is a line. If (x,y) is 
+if (a,b,c) is returned, then the solution is a line. If (x,y) is
 returned, the solution is a point, if False then no solution.
 
 Time Complexity: O(1)
 Space Complexity: O(1)
 '''
-#This method is not completely verified.
 def lineline(line1,line2):
     a1,b1,c1 = line1
     a2,b2,c2 = line2
@@ -18,16 +18,16 @@ def lineline(line1,line2):
         if b1*c2 == b2*c1: return (a1,b1,c1)
         else: return False
     elif a1 == 0:
-        return ((-c2+b2*c1)/(a2*b1),-c1/b1) 
+        return (b2*c1/(a2*b1)-c2/a2,-c1/b1)
     elif a2 == 0:
-        return ((-c1+c2*b1)/(b2*a1),-c2/b2)
+        return (b1*c2/(a1*b2)-c1/a1,-c2/b2)
     elif b1 == b2 == 0:
         if a1*c2 == a2*c1: return (a1,b1,c1)
         else: return False
     elif b1 == 0:
-        return (-a2*c1/(b2*a1)-c2,c1/a1)
+        return (-c1/a1, (a2*c1 - a1*c2) / (a1*b2))
     elif b2 == 0:
-        return (-a1*c2/(b1*a2)-c2,c2/a2)
+        return (-c2/a2, (a1*c2 - a2*c1) / (a2*b1))
     elif a1/b1 == a2/b2:
         if c1/b1 == c2/b2: return (a1,b1,c1)
         else: return False
@@ -36,12 +36,13 @@ def lineline(line1,line2):
         y = -a1*x/b1-c1/b1
         return (x,y)
 
-#If (x,y) is returned, this is the intersection point. 
+#If (x,y) is returned, this is the intersection point.
 #Otherwise False is returned.
-#This method is not entirely verified.
 def lineseg(line1,seg):
     line2 = twopointstoline(*seg)
     intersection = lineline(line1,line2)
+    if intersection == False:
+        return False
     if len(intersection) == 3:
         return (seg[0],seg[1])
     elif len(intersection) == 2:
@@ -50,10 +51,9 @@ def lineseg(line1,seg):
             return (x,y)
         return False
     else:
-        return False
+        assert(False)
 
 #The orthogonal projection is returned.
-#This method is not entirely verified.
 def linepoint(line,p):
     a,b,c = line
     x,y = p
@@ -61,7 +61,6 @@ def linepoint(line,p):
             (a*(-b*x+a*y)-b*c)/(a**2+b**2))
 
 #Returns intersection point if it exists.
-#This method is not entirely verified.
 def segseg(seg1,seg2):
     line1 = twopointstoline(*seg1)
     line2 = twopointstoline(*seg2)
@@ -70,9 +69,8 @@ def segseg(seg1,seg2):
     return False
 
 #Returns the point closest to p on the segment seg.
-#This method is verified by Lars (problem halloweemirrors)
 def segpoint(seg,p):
-    line = twopointstoline(*seg)
+    line = twopointstoline(seg)
     p2 = linepoint(line,p)
     if p2 and (p2[0]-seg[0])*(p2[0]-seg[2]) <= 0 and \
             (p2[1]-seg[1])*(p2[1]-seg[3]) <= 0:
@@ -83,10 +81,11 @@ def segpoint(seg,p):
         else: return (seg[2],seg[3])
 
 #Returns the distance between p1 and p2.
-#This method is correct.
 def dist(p1,p2):
     return ((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)**0.5
 
 #Returns the line as (a,b,c) from two points.
 def twopointstoline(x1,y1,x2,y2):
     return (y2-y1,x1-x2,x2*y1-x1*y2)
+
+
