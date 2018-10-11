e6953642c4b48b9a9e8d27840c335039ea51fe62
LarsAstrom <astrom.lars@telia.com>
2018-10-11 20:55:19 +0200

    fixed bugs in sgmtree, checked it with kattis problem

--- code/sgmtree.cpp
+++ code/sgmtree.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class sgmtree {
 public:
   vector<int> vals;
@@ -10,33 +13,33 @@ public:
     build(1,0,n-1);
   }
   int que(int L, int R) {
-    return(1,0,n-1,L,R);
+    return que(1,0,n-1,L,R);
   }
   void update(int ind, int val) {
     vals[ind]=val;
     update(1,0,n-1,ind);
   }
 private:
-  int I = -9999999; // I
+  int I = 0; // I
   void build(int node, int l, int r) {
     if (l==r) {tree[node]=vals[l]; return;}
     int mid=(l+r)/2;
     build(2*node,l,mid);
     build(2*node+1,mid+1,r);
-    tree[node]=max(tree[2*node],tree[2*node+1]); // op
+    tree[node]=tree[2*node]+tree[2*node+1]; // op
   }
   int que(int node, int l, int r, int L, int R) {
     if (l>R || r<L) return I; // I
     if (l>=L && r<=R) return tree[node];
     int mid=(l+r)/2;
-    return max(que(2*node,l,mid,L,R),que(2*node+1,mid+1,r,L,R)); // op
+    return que(2*node,l,mid,L,R)+que(2*node+1,mid+1,r,L,R); // op
   }
   void update(int node, int l, int r, int ind) {
-    if (l==r && l==ind) {tree[node]=vals; return;}
+    if (l==r && l==ind) {tree[node]=vals[ind]; return;}
     if (l>ind || r<ind) return;
     int mid=(l+r)/2;
     update(2*node,l,mid,ind);
     update(2*node+1,mid+1,r,ind);
-    tree[node]=max(tree[2*node],tree[2*node+1]); // Op
+    tree[node]=tree[2*node]+tree[2*node+1]; // Op
   }
 };
