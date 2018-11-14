78cfd13eae757bba9e2b4d3a66bbc98f684733fd
LarsAstrom <astrom.lars@telia.com>
2018-11-14 20:24:12 +0100

    something small

--- code/knapsack.py
+++ code/knapsack.py
@@ -1,14 +1,3 @@
-'''
-Returns the solution to the knapsack problem with weights w, values v
-and total capacity W. DP[i][j] is the total value when taking a the i
-first objects with a total weight of at most j. The solution is found in
-DP[len(w)], where the specific problem will have different places to
-find the solution. NOTE THAT SOMETIMES, IT IS MORE EFFICIENT TO STORE
-MAPS AT EACH LEVEL IN THE DP.
-
-Time Complexity: O(n*W)
-Space Complexity: O(n*W)
-'''
 def knapsack(w, v, W):
     n = len(w)
     DP = [[0]*(W+1) for _ in range(n+1)]
