ca323782e8c0bbd212044514f2845828e8a75f3c
Bjorn <bjorn.in.magnusson@gmail.com>
2018-11-14 16:23:59 +0100

    added sync_with_stdio(false) in template

--- code/template.cpp
+++ code/template.cpp
@@ -4,6 +4,7 @@ typedef long long ll;
 #define rep(i,a,b) for (ll i = a; i<ll(b); i++)
 //compile with g++/cc -g -Wall -Wconversion -fsanitize=address,undefined <filename.cpp>s
 int main() {
+  ios::sync_with_stdio(false);
   cout << setprecision(10);
 
 }
