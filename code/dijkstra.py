222e869c769dca2b417f78c2f4345829c25155d3
LarsAstrom <astrom.lars@telia.com>
2019-05-19 14:50:26 +0200

    dijkstra clean up

---
+++ code/dijkstra.py
@@ -0,0 +1,28 @@
+'''
+Implementation of djikstras algorithm. Finds the shortest path from a 
+source, to all other nodes (non-negative weights).
+adj is a list of adjacency lists and s the source node.
+
+Time Complexity: O(M + NlogN), where N is the number of nodes, M edges.
+Space Complexity: O(M + N)
+'''
+from heapq import heappush, heappop
+
+INF = 10**12
+def djikstra(adj,S):
+    N = len(adj)
+    d = [INF]*N
+    vis = [False]*N
+    d[S],vis[S] = 0,True
+    pq = [] 
+    heappush(pq, (d[S],S))
+    while pq:
+        curD, curN = heappop(pq)
+        if vis[curN]: continue
+        vis[curN] = True
+        for ne,w in adj[curN]:
+            altD = curD + w
+            if altD < d[ne]:
+                heappush(pq,(altD,ne))
+                d[ne] = altD
+    return d                                                                                                                                
